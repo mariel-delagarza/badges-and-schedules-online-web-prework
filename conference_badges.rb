f60772477790dacf13831a5dcbd879ffa5b335d2
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-11 16:53:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,7 @@ end
 def print(attendees)
    batch_badge_creator(attendees).each do |attendee|
      puts attendee
+   end 
    assign_rooms(attendees).each do |attendee|
      puts attendee
   end 
