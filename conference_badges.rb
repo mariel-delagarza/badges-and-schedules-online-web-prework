da55e7d2e568870b64b35a85975a2471ad286fa8
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-11 16:50:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(array)
 end 
 
 def assign_rooms(attendees) 
-    attendees.each_with_index do |attendee, index|
+    attendees.each_with_index.map do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end 
 end 
